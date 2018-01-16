6d0ddeadc87d4d837befd44cce9c1c908e650ae1
brickhouse <mmaurier1@gmail.com>
2018-01-15 23:39:39 -0500

    add js file, move js code of timeline.html

--- verses.js
+++ verses.js
@@ -1,3 +1,4 @@
+// adam to flood
 var gen5_3 = "Genesis 5:3\nWhen Adam had lived 130 years,\nhe fathered a son in his own likeness,\nafter his image, and named him Seth.";
 var gen5_6 = "Genesis 5:6\nWhen Seth had lived 105 years,\nhe fathered Enosh.";
 var gen5_9 = "Genesis 5:9\nWhen Enosh had lived 90 years,\nhe fathered Kenan.";
@@ -8,7 +9,7 @@ var gen5_21 = "Genesis 5:21\nWhen Enoch had lived 65 years,\nhe fathered Methuse
 var gen5_25 = "Genesis 5:25\nWhen Methuselah had lived 187 years,\nhe fathered Lamech.";
 var gen5_28 = "Genesis 5:28,29\nWhen Lamech had lived 182 years,\nhe fathered ... Noah";
 var gen7_6 = "Genesis 7:6\nNoah was six hundred years old\nwhen the flood of waters came upon the earth.";
-
+// flodd to exodus
 var gen11_10 = "Genesis 11:10\nThese are the generations of Shem...\nhe fathered Arpachshad two years after the flood.";
 var gen11_12 = "Genesis 11:12\nWhen Arpachshad had lived 35 years,\nhe fathered Shelah";
 var gen11_14 = "Genesis 11:14\nWhen Shelah had lived 30 years,\nhe fathered Eber.";
@@ -18,5 +19,14 @@ var gen11_20 = "Genesis 11:20\nWhen Reu had lived 32 years,\nhe fathered Serug."
 var gen11_22 = "Genesis 11:22\nWhen Serug had lived 30 years,\nhe fathered Nahor.";
 var gen11_24 = "Genesis 11:24\nWhen Nahor had lived 29 years,\nhe fathered Terah.";
 var gen11_32 = "Genesis 11:32\nThe days of Terah were 205 years,\nand Terah died in Haran.";
+var gen12_1 = "Genesis 12:1\nNow the Lord said to Abram,\n\“Go from your country and your kindred and your father's house\nto the land that I will show you.";
+var gen12_4 = "Genesis 12:4\n So Abram went, as the Lord had told him,\nand Lot went with him. Abram was seventy-five years old\nwhen he departed from Haran.";
+var acts7_4 = "Acts 7:4\nThen he went out from the land of the Chaldeans\nand lived in Haran. And after his father died,\nGod removed him from there into this land\nin which you are now living.";
 var gen21_5  = "Genesis 21:5\nAbraham was a hundred years old\nwhen his son Isaac was born to him.";
 var gen25_26 = "Genesis 25:26\nAfterward his brother came out\n... so his name was called Jacob.\n Isaac was sixty years old when she bore them\n";
+var gen41_46 = "Genesis 41:46\nJoseph was thirty years old when\hhe entered the service of Pharaoh king of Egypt.";
+var gen41_47 = "Genesis 41:47-48\n During the seven plentiful years the earth produced abundantly,";
+var gen45_4 = "Genesis 45:4\nSo Joseph said to his brothers,\“Come near to me, please.\”";
+var gen45_6 = "Genesis 45:6\nFor the famine has been in the land these two years,\nand there are yet five years in which there will be\nneither plowing nor harvest.\n";
+var gen45_7 = "Genesis 45:7\nAnd God sent me before you to preserve for you a remnant on earth,\nand to keep alive for you many survivors.";
+var gen47_9 = "Genesis 47:9\nAnd Jacob said to Pharaoh,\n\"The days of the years of my sojourning are 130 years.\"";
