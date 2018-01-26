3253747614b710b62c8897e98341263e0bd16578
brickhouse <mmaurier1@gmail.com>
2018-01-25 22:37:46 -0500

    add exodus to solomon vars and verses

---
+++ variables.js
@@ -0,0 +1,14 @@
+var sums = ["130 years" , "235 years" , "325 years" , "395 years" , "460 years" , "622 years" , "687 years" , "874 years" , "1056 years" , "1656 years"];		
+var sums1 = ["2 years" , "37 years" , "67 years" , "101 years" , "131 years" , "163 years" , "193 years" , "222 years" , 
+             "352 years" , "452 years" , "512 years" , "603 years" , "642 years", "857 years"];
+
+var states = [false, false, false, false, false, false, false, false, false, false, false,
+			  false, false, false, false, false, false, false, false, false, false, false, 
+			  false, false, false, false, false, false, false, false, false, false, false, 
+			  false, false, false, false, false, false, false, false, false, false, false,
+			  false, false, false, false, false, false, false, false, false, false, false,
+			  false, false, false, false, false, false, false, false, false, false, false,
+			  false, false, false, false, false, false, false, false, false, false, false];	
+
+var color = 0xCFEBF9, color1 = 0x2382B2;
+var counter = 0;			  
