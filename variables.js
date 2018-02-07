3c898e6d49d746ab0b3692d7bb10d940d8853e1e
brickhouse <mmaurier1@gmail.com>
2018-02-06 22:47:06 -0500

    add final scene

--- variables.js
+++ variables.js
@@ -1,5 +1,8 @@
 var camera, scene, loader, renderer;
 var titleTM, verseTM, sumTM, totalTM;
+var title0TM, title1TM, title2TM, title3TM, title4TM, title5TM, title6TM;
+var gt0TM, gt1TM, gt2TM, gt3TM, gt4TM, gt5TM, gt6TM;
+var grandTotals = ["1656","857","516","393","586","2018","6026"];
 var sums0 = [ "130" , 
              "105  +   130  =  235" , 
              "90   +   235  =  325" , 
@@ -47,14 +50,13 @@ var sums3 = [ "17" ,
               "11  +  371   =  382",
               "3 month  +  382 = 382",
               "11  +  382  =  393" ];  
-var sums4 = ["587","-1 + 587 = 586","2018"];  
+var sums4 = ["587","-1 + 587 = 586","2018", "6026"];  
 
 var total0 = ["130 years","235 years","325 years","395 years","460 years","622 years","687 years","874 years","1056 years","1656 years"];
 var total1 = ["2 years","37 years","67 years","101 years","131 years","163 years","193 years","222 years","352 years","452 years","512 years","603 years","642 years","857 years"];
 var total2 = ["480 years","487 years","516 years"];
 var total3 = ["17 years","20 years","61 years","85 years","94 years","95 years","101 years","141 years","170 years","222 years","238 years","254 years","283 years","338 years","340 years","371 years","371 years","382 years","382 years","393 years"];             
 var total4 = ["587 years","586 years","2018"];          
-var grandTotals = ["1656","857","","516","393","586","2018","6026"];
                         
 var titles = [  "Years from Adam to the Flood:", 
                 "Years from the Flood to Exodus:" , 
@@ -62,7 +64,8 @@ var titles = [  "Years from Adam to the Flood:",
                 "From King Solomon to the Exile:", 
                 "From the Exile to Common Era:", 
                 "From Start of Common Era to Today:",
-                "Years from Adam until Today:" ];           
+                "Years from Adam until Today:",
+                "Years Since the Creation of Adam:" ];           
 var fadeVerseSum = false; 
 var fadeSum = true;
 var freezeTotal = false;
@@ -99,4 +102,4 @@ var cArr = [100,500,600,1000,
             24100,24500,24600,25000,
             25100,25500,25600,26000,
             100,500,600,27000,100,500,600,1000,100,500,600,1000,100,500,600,1000,
-            100,500,600,1000,100,500,600,1000,100,500,600,1000,100,500,600,1000,100,500,600,1000]
+            100,500,600,1000,100,500,600,1000,100,500,600,1000,100,500,600,1000,100,500,600,1000];
